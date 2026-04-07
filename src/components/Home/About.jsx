4579fbb42cdb2f66cff4c34c56b244c7c8fd513f
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2026-04-08 02:40:19 +0600

    2 cards add done

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// ABOUT.JSX FILE
 
 const About = () => {
     return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import About from '../components/Home/About';
+import Portfolio from '../components/Home/Portfolio';
 
 const Home = () => {
     return (
         <div className='max-w-7xl mx-auto space-y-4'>
-            <About/>
+            <About />
+            <Portfolio/>
         </div>
     );
 
